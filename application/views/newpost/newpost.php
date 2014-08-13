905e62799721343aff1ee40521c950333f8d193a
icyflame <kannan.siddharth12@gmail.com>
2014-08-13 12:36:13 +0530

    Incorporated the post title.
    Screen name will default to the name of the user, and this can't be changed.

--- application/views/newpost/newpost.php
+++ application/views/newpost/newpost.php
@@ -15,17 +15,23 @@
 
 	<?php echo form_open('newpostcont/addpost') ?>
 
-	<input type="text" class="form-control" name="screenname" placeholder="Screen name" value="<?php echo $this->session->userdata('fullname'); ?>"></input>
+	<input type="text" class="form-control" name="screenname" value="<?php echo $this->session->userdata('fullname'); ?>" style="display: none; "></input>
 
+	<input type="text" class="form-control" name="screenname" placeholder="Screen name" value="<?php echo $this->session->userdata('fullname'); ?>" disabled></input>
+	<br/>
+
+	<input type="text" class="form-control" name="datetime" placeholder="Current date and time" value="<?php echo date("H:i jS F, Y"); ?>" disabled></input>
+	<br/>
+
+	<input type="text" class="form-control" name="posttitle" placeholder="Post Title"></input>
 	<br/>
 
 	<textarea class="form-control" name="postcontent" rows="6" placeholder="Enter your post here"></textarea>
-
 	<br/>
 
 	<input type="submit" value="Submit" class="btn btn-primary btn-lg"></input>
 
-	</form>
+</form>
 
 </body>
 
